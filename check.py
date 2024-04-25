2e8289960f6538509b129594c10e6d94692d30d6
Iskandar Verdiyev <101502402+IskaIV@users.noreply.github.com>
2024-04-24 21:57:00 -0400

    Update check.py

--- check.py
+++ check.py
@@ -2,11 +2,16 @@ import hashlib
 secret_mod = 18446744073709551615
 
 # Doesn't actually add any cryptographic complexity
-def generate_token(role):
-    hashed_role = int(hashlib.sha256(role.encode()).hexdigest(), 16) % secret_mod
+
+
+def generate_token(WorkID):
+    hashed_role = int(hashlib.sha256(
+        WorkID.encode()).hexdigest(), 16) % secret_mod
     return str(hashed_role)
 
-def check_token(token, role):
-    hashed_role = int(hashlib.sha256(role.encode()).hexdigest(), 16) % secret_mod
+
+def check_token(token, WorkID):
+    hashed_role = int(hashlib.sha256(
+        WorkID.encode()).hexdigest(), 16) % secret_mod
     token = int(token)
     return hashed_role == token
