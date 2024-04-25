d6659c343178fab27d1290fe9742831125930d69
Iskandar Verdiyev <101502402+IskaIV@users.noreply.github.com>
2024-04-24 20:27:56 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+def start_db():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    # Create the Login table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Login (
+            WORKID TEXT,
+            Password TEXT,
+            First TEXT,
+            Last TEXT
+        )
+    ''')
+
+    # Create the Files table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Posts (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            WORKID TEXT,
+            Title TEXT,
+            Description TEXT
+        )
+    ''')
+
+    # Create the Replies table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Replies (
+            PrimaryUser TEXT,
+            SecondaryUser TEXT,
+            Message TEXT,
+            Ind INTEGER PRIMARY KEY AUTOINCREMENT
+        )
+    ''')
+
+    # Create the Comments table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Comments (
+            PostId INTEGER,
+            CommentId INTEGER PRIMARY KEY AUTOINCREMENT,
+            Content TEXT,
+            Author TEXT
+        )
+    ''')
+    cursor.close()
